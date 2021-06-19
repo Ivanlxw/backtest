3af83d74ee9aad16f6044fc653c2b456ab39c2cc
Ivanlxw <leexianwei96@gmail.com>
2021-06-19 11:42:11 +0800

    refresh data and filter by sector

---
+++ data/fmp.py
@@ -0,0 +1,83 @@
+import os
+import json
+
+from backtest.utilities.utils import load_credentials, parse_args, remove_bs
+import requests
+
+BASE_URL = "https://financialmodelingprep.com/api/v3/"
+with open(f"{os.path.dirname(__file__)}/dow_stock_list.txt", "r") as fin:
+    stock_list = fin.readlines()
+dow_stock_list = list(map(remove_bs, stock_list))
+
+
+def parseFmpScreenerRes(res_json: list):
+    final_stocks = []
+    for ticker in res_json:
+        # market cap > 50m and from US
+        if ticker['marketCap'] > 50000000 and ticker['country'] == "US":
+            final_stocks.append(ticker['symbol'])
+    return final_stocks
+
+
+def getindustryByStock():
+    industry_by_stock = {}
+    for stock in dow_stock_list:
+        res = requests.get(
+            f"https://financialmodelingprep.com/api/v3/profile/{stock}?apikey={os.environ['FMP_API']}")
+        if res.ok:
+            # print(res.json())
+            try:
+                res = res.json()[0]
+                if res["industry"] in industry_by_stock.keys():
+                    industry_by_stock[res["industry"]].add(stock)
+                else:
+                    industry_by_stock[res["industry"]] = {stock}  # set
+            except Exception as e:
+                print(res.json())
+                print(e)
+
+    # convert dict of set to dict of list
+    for k in industry_by_stock.keys():
+        industry_by_stock[k] = list(industry_by_stock[k])
+
+    curr_data = {}
+    with open("industry_by_symbols.json", "r+") as f:
+        curr_data = json.load(f)
+
+    with open("industry_by_symbols.json", "w") as f:
+        # combine curr_data and industry_by_stock
+        for k in curr_data.keys():
+            if k in industry_by_stock.keys():
+                industry_by_stock[k] += curr_data[k]
+            else:
+                industry_by_stock[k] = curr_data[k]
+        json.dump(industry_by_stock, f)
+
+
+def getScreenedStocks(marketCapMin=None):
+    base_url = BASE_URL + "stock-screener?country=US"
+    if marketCapMin is not None:
+        base_url += f"&marketCapMoreThan={marketCapMin}"
+    final_url = base_url + f"&apikey={os.environ['FMP_API']}"
+    res = requests.get(final_url)
+    if res.ok:
+        return res.json()
+
+
+args = parse_args()
+load_credentials(args.credentials)
+
+sectors = [
+    "Consumer Cyclical", "Energy", "Technology", "Industrials", "Financial Services",
+    "Basic Materials", "Communication Services", "Consumer Defensive", "Healthcare", "Real Estate",
+    "Utilities", "Industrial Goods", "Financial", "Services", "Conglomerates"
+]
+industries = [
+    "Autos", "Banks", "Banks Diversified", "Software",
+    "Banks Regional", "Beverages Alcoholic", "Beverages Brewers", "Beverages Non-Alcoholic"]
+
+stockList = getScreenedStocks(marketCapMin=50000000)
+stockList = [stock for stock in stockList if stock['isActivelyTrading'] == True]
+stockList = list(map(lambda x: x["symbol"], stockList))
+with open(f"{os.path.dirname(__file__)}/stock_universe.txt", "w") as f:
+    f.write("\n".join(stockList))
--- data/get_csv.py
+++ data/get_csv.py
@@ -3,72 +3,104 @@ import pandas as pd
 import os
 from datetime import datetime
 
+from backtest.utilities.utils import parse_args, load_credentials
+
+args = parse_args()
+load_credentials(args.credentials)
+
+
 def get_av_csv(symbol, csv_dir, key, full=False, interval=None,):
     print(f"Getting symbol: {symbol}")
     if interval != None:
         if interval not in ['1min', '5min', '15min', '30min', '60min']:
-            raise Exception("Interval has to be one of the following options (string):\n'1min', '5min', '15min', '30min', '60min'")
+            raise Exception(
+                "Interval has to be one of the following options (string):\n'1min', '5min', '15min', '30min', '60min'")
 
         if full:
-            url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=" + interval +"&outputsize=full" + "&apikey=" + key
-        else: 
-            url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=" + interval + "&apikey=" + key            
-        
+            url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + \
+                symbol + "&interval=" + interval + "&outputsize=full" + "&apikey=" + key
+        else:
+            url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + \
+                symbol + "&interval=" + interval + "&apikey=" + key
+
         if not os.path.exists(f"{interval}"):
             os.mkdir(f"{interval}")
 
         filepath = f"./{interval}/{symbol}.csv"
     else:
         if full:
-            url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + symbol + "&outputsize=full" + "&apikey=" + key
-        else: 
-            url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + symbol + "&apikey=" + key
-        
+            url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + \
+                symbol + "&outputsize=full" + "&apikey=" + key
+        else:
+            url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + \
+                symbol + "&apikey=" + key
+
         if not os.path.exists(csv_dir):
             os.mkdir(csv_dir)
 
         filepath = csv_dir + f"/{symbol}.csv"
 
     parsed_data = requests.get(url).json()
-    df = pd.DataFrame.from_dict(parsed_data['Time Series (Daily)'], orient='index')
-    df = df.iloc[::-1]  ## reverse from start to end instead of end to start
+    df = pd.DataFrame.from_dict(
+        parsed_data['Time Series (Daily)'], orient='index')
+    df = df.iloc[::-1]  # reverse from start to end instead of end to start
     df.columns = ["open", "high", "low", "close", "volume"]
     merge_n_save(filepath, df)
 
-def get_tiingo_eod(ticker, fp, full:bool, key):
+
+def get_tiingo_eod(ticker, fp, full: bool, key):
     headers = {
-            'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
     }
     if full:
-            start_date = "2000-1-1"
-            end_date = datetime.today().strftime('%Y-%m-%d')
+        start_date = "2000-1-1"
+        end_date = datetime.today().strftime('%Y-%m-%d')
     else:
-            start_date = "2020-1-1"
-            end_date = datetime.today().strftime('%Y-%m-%d')
+        start_date = "2020-1-1"
+        end_date = datetime.today().strftime('%Y-%m-%d')
 
     url = f"https://api.tiingo.com/tiingo/daily/{ticker}/prices?startDate={start_date}&endDate={end_date}&resampleFreq=daily&token={key}"
 
     requestResponse = requests.get(url, headers=headers)
-    json_df = requestResponse.json()
-    df = pd.DataFrame(json_df)
-    df['date'] = df['date'].apply(lambda x: x.replace("T00:00:00.000Z", ""))
-    df.index = df['date']
-    df = df.drop("date", axis=1)
-    df = df[["open", "high", "low", "close", "volume"]]
-    merge_n_save(fp, df)
+    if requestResponse.ok:
+        json_df = requestResponse.json()
+        try:
+            df = pd.DataFrame(json_df)
+            df['date'] = df['date'].apply(
+                lambda x: x.replace("T00:00:00.000Z", ""))
+            df.index = df['date']
+            df = df.drop("date", axis=1)
+            df = df[["open", "high", "low", "close", "volume"]]
+            merge_n_save(fp, df)
+        except Exception as e:
+            print(json_df)
+            print(e)
+
 
 def merge_n_save(filepath, df):
-    daily_fp =os.path.join(os.getcwd(), 'data','data', 'daily')
+    daily_fp = os.path.join(os.getcwd(), 'data', 'data', 'daily')
     if not os.path.exists(daily_fp):
         os.makedirs(daily_fp)
     if os.path.exists(filepath):
-        ## merge data
+        # merge data
         existing_df = pd.read_csv(filepath, index_col=0)
         df = pd.concat([existing_df, df])
         df = df[~df.index.duplicated(keep='last')]
         df.sort_index(inplace=True)
         assert df.index.nunique() == df.index.size
+    df.to_csv(os.path.join(daily_fp, filepath))
+    print("Data is stored at {}".format(os.path.join(daily_fp, filepath)))
 
-    df.to_csv(filepath)
-    print("Data is stored at {}".format(filepath))
-    
+
+def refresh_data(tiingo_key):
+    from backtest.utilities.utils import remove_bs
+    with open(f"{os.path.dirname(__file__)}/dow_stock_list.txt", "r") as fin:
+        stock_list = fin.readlines()
+    dow_stock_list = list(map(remove_bs, stock_list))
+
+    for ticker in dow_stock_list:
+        get_tiingo_eod(ticker, f"{ticker}.csv", full=True,
+                       key=tiingo_key)
+
+
+refresh_data(os.environ["TIINGO_API"])
