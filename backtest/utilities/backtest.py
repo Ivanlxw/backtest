c8132ff19aa958d295f155103f98f3f40df09176
Ivanlxw <leexianwei96@gmail.com>
2021-12-11 11:33:21 +0800

    refactor - backtest loop logic to backtest.py

--- backtest/utilities/backtest.py
+++ backtest/utilities/backtest.py
@@ -1,35 +1,128 @@
 import datetime
+import time
 import queue
 import copy
+
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from backtest.broker import SimulatedBroker
 from trading.portfolio.portfolio import PercentagePortFolio
-from trading.strategy.naive import BuyAndHoldStrategy
-from backtest.utilities.utils import _backtest_loop, _life_loop
+from trading.strategy.base import BuyAndHoldStrategy
+from backtest.utilities.utils import log_message
 from trading.data.dataHandler import DataHandler, HistoricCSVDataHandler
 from trading.utilities.enum import OrderType
 from trading.utilities.constants import benchmark_ticker
+from trading.plots.plot import Plot
+
+NY = "America/New_York"
+
+def _backtest_loop(bars, event_queue, order_queue, strategy, port, broker) -> Plot:
+    start = time.time()
+    while True:
+        # Update the bars (specific backtest code, as opposed to live trading)
+        if bars.continue_backtest == True:
+            bars.update_bars()
+        else:
+            while not event_queue.empty():
+                event_queue.get()
+            break
+        while True:
+            try:
+                event = event_queue.get(block=False)
+            except queue.Empty:
+                break
+            else:
+                if event is not None:
+                    if event.type == 'MARKET':
+                        port.update_timeindex()
+                        signal_list = strategy.calculate_signals(event)
+                        for signal in signal_list:
+                            if signal is not None:
+                                event_queue.put(signal)
+                        while not order_queue.empty():
+                            event_queue.put(order_queue.get())
+
+                    elif event.type == 'SIGNAL':
+                        port.update_signal(event)
+
+                    elif event.type == 'ORDER':
+                        if broker.execute_order(event):
+                            log_message(event.order_details())
+
+                    elif event.type == 'FILL':
+                        port.update_fill(event)
+
+    print(f"Backtest finished in {time.time() - start}. Getting summary stats")
+    port.create_equity_curve_df()
+    log_message(port.output_summary_stats())
+    plotter = Plot(port)
+    plotter.plot()
+    return plotter
+
+
+def _life_loop(bars, event_queue, order_queue, strategy, port, broker, sleep_duration: datetime.timedelta) -> Plot:
+    while True:
+        # Update the bars (specific backtest code, as opposed to live trading)
+        now = pd.Timestamp.now(tz=NY)
+        if now.dayofweek >= 4 and now.hour > 17:
+            break
+        time_since_midnight = now - now.normalize()
+        # only run during trading hours -> 0945 - 1745
+        if (time_since_midnight < datetime.timedelta(hours=9, minutes=45) or time_since_midnight > datetime.timedelta(hours=17, minutes=45)):
+            continue
+        log_message("update_bars()")
+        bars.update_bars()
+        while True:
+            try:
+                event = event_queue.get(block=False)
+            except queue.Empty:
+                break
+            else:
+                if event is not None:
+                    if event.type == 'MARKET':
+                        log_message("MarketEvent")
+                        port.update_timeindex()
+                        signal_list = strategy.calculate_signals(event)
+                        for signal in signal_list:
+                            if signal is not None:
+                                event_queue.put(signal)
+                        while not order_queue.empty():
+                            event_queue.put(order_queue.get())
+
+                    elif event.type == 'SIGNAL':
+                        port.update_signal(event)
+
+                    elif event.type == 'ORDER':
+                        if broker.execute_order(event):
+                            logging.info(event.order_details())
+
+                    elif event.type == 'FILL':
+                        port.update_fill(event)
+        log_message("sleeping")
+        time.sleep(sleep_duration.total_seconds())  # 18 hrs
+        log_message("sleep over")
 
 
 def backtest(bars: DataHandler, event_queue, order_queue,
              strategy, port, broker,
              start_date=None,
              loop_live: bool = False,
-             show_plot: bool=True,
+             show_plot: bool = True,
              sleep_duration: int = 86400):
     if not loop_live and start_date is None:
         raise Exception("If backtesting, start_date is required.")
 
     if loop_live:
-        _life_loop(bars, event_queue, order_queue, strategy, port, broker, datetime.timedelta(seconds=sleep_duration))
+        _life_loop(bars, event_queue, order_queue, strategy, port,
+                   broker, datetime.timedelta(seconds=sleep_duration))
     else:
         benchmark_strat_bars = copy.copy(bars)
         _backtest_loop(bars, event_queue, order_queue, strategy, port, broker)
         plot_benchmark(symbol_list=bars.symbol_list,
-                    portfolio_name="benchmark_strat", benchmark_bars=benchmark_strat_bars)
+                       portfolio_name="benchmark_strat", benchmark_bars=benchmark_strat_bars)
         plot_benchmark(symbol_list=[benchmark_ticker],
-                    portfolio_name="benchmark_index", benchmark_bars=None, start_date=start_date)
+                       portfolio_name="benchmark_index", benchmark_bars=None, start_date=start_date)
         if show_plot:
             plt.legend()
             plt.show()
--- backtest/utilities/utils.py
+++ backtest/utilities/utils.py
@@ -1,17 +1,11 @@
 import json
-import datetime
 import argparse
 from pathlib import Path
-import time
-import queue
 import logging
 import os
 import random
 import pandas as pd
-from trading.plots.plot import Plot
-from trading.utilities.constants import backtest_basepath
 
-NY = "America/New_York"
 UTILS_ABS_FP = Path(os.path.dirname(os.path.abspath(__file__)))
 MODELINFO_DIR = UTILS_ABS_FP / "../../Data/strategies"
 if not os.path.exists(MODELINFO_DIR):
@@ -28,7 +22,7 @@ def parse_args():
                         help='inform life?')
     parser.add_argument("--num-runs", type=int, default=1, help="Run backtest x times, get more aggregated performance details from log")
     parser.add_argument("--frequency", type=str, default="daily", help="Frequency of data. Searches a dir with same name")
-    parser.add_argument("--sleep-time", type=int, default=43200, help="Sleep time in seconds")
+    parser.add_argument("--sleep-time", type=int, default=43200, help="Sleep time in seconds. Defaults to sleep time in live loop")
     return parser.parse_args()
 
 
@@ -54,87 +48,3 @@ def generate_start_date():
 
 def log_message(message: str):
     logging.info(f"{pd.Timestamp.now()}: {message}")
-
-def _backtest_loop(bars, event_queue, order_queue, strategy, port, broker) -> Plot:
-    start = time.time()
-    while True:
-        # Update the bars (specific backtest code, as opposed to live trading)
-        if bars.continue_backtest == True:
-            bars.update_bars()
-        else:
-            while not event_queue.empty():
-                event_queue.get()
-            break
-        while True:
-            try:
-                event = event_queue.get(block=False)
-            except queue.Empty:
-                break
-            else:
-                if event is not None:
-                    if event.type == 'MARKET':
-                        port.update_timeindex()
-                        signal_list = strategy.calculate_signals(event)
-                        for signal in signal_list:
-                            if signal is not None:
-                                event_queue.put(signal)
-                        while not order_queue.empty():
-                            event_queue.put(order_queue.get())
-
-                    elif event.type == 'SIGNAL':
-                        port.update_signal(event)
-
-                    elif event.type == 'ORDER':
-                        if broker.execute_order(event):
-                            logging.info(event.order_details())
-
-                    elif event.type == 'FILL':
-                        port.update_fill(event)
-
-    print(f"Backtest finished in {time.time() - start}. Getting summary stats")
-    port.create_equity_curve_df()
-    logging.log(32, port.output_summary_stats())
-    plotter = Plot(port)
-    plotter.plot()
-    return plotter
-
-
-def _life_loop(bars, event_queue, order_queue, strategy, port, broker, sleep_duration: datetime.timedelta) -> Plot:
-    while True:
-        # Update the bars (specific backtest code, as opposed to live trading)
-        now = pd.Timestamp.now(tz=NY)
-        if now.dayofweek >= 4 and now.hour > 17:
-            break
-        if not ((now.hour >= 9 and now.minute > 45) and now.hour < 18):  # only run during trading hours -> 0945 - 1805
-            continue
-        log_message("update_bars()")
-        bars.update_bars()
-        while True:
-            try:
-                event = event_queue.get(block=False)
-            except queue.Empty:
-                break
-            else:
-                if event is not None:
-                    if event.type == 'MARKET':
-                        logging.info(f"{now}: MarketEvent")
-                        port.update_timeindex()
-                        signal_list = strategy.calculate_signals(event)
-                        for signal in signal_list:
-                            if signal is not None:
-                                event_queue.put(signal)
-                        while not order_queue.empty():
-                            event_queue.put(order_queue.get())
-
-                    elif event.type == 'SIGNAL':
-                        port.update_signal(event)
-
-                    elif event.type == 'ORDER':
-                        if broker.execute_order(event):
-                            logging.info(event.order_details())
-
-                    elif event.type == 'FILL':
-                        port.update_fill(event)
-        log_message("sleeping")
-        time.sleep(sleep_duration.total_seconds())  # 18 hrs
-        log_message("sleep over")
